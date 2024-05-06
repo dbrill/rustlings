df4d9aae49a1a69470519311acb27c66d8528fb0
Danny Brill <dannybrill@meta.com>
2024-05-06 09:50:32 -0400

    exercises 2-7

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,12 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+	// originally I used a vanilla Array like ["test"; 200], which creates an array filled with 200 instances of the string "test"
+	// Then I tried out this fancy Vec syntax. Vecs are dynamically sized and so they go on the heap whereas Arrays are fixed size
+	// and go on the stack.
+    let a = (0..200).collect::<Vec<u32>>(); 
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
