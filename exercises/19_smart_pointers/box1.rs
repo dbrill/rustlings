afe88e718edc7454f5dcefa249ef2cddfa421a03
Danny Brill <dannybrill@meta.com>
2024-05-28 22:02:37 +0100

    exercise 19

--- exercises/19_smart_pointers/box1.rs
+++ exercises/19_smart_pointers/box1.rs
@@ -16,13 +16,10 @@
 //
 // Note: the tests should not be changed
 //
-// Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
+// Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.  #[derive(PartialEq, Debug)]
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -35,11 +32,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    List::Cons(7, Box::new(List::Nil))
 }
 
 #[cfg(test)]
