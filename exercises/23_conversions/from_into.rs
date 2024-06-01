fbd6022bd0c46ae247ad8021a501abdba4041d7e
Danny Brill <dannybrill@meta.com>
2024-06-01 11:40:02 -0400

    exercise 23 -- finished!

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -41,10 +41,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {}
+    fn from(s: &str) -> Person {
+        let parts: Vec<&str> = s.split(',').collect();
+	
+	println!("Here: {:?}", parts);
+	if parts.len() < 2 || parts.len() > 2 || parts[0] == ""{
+		return Person::default();
+	}
+	let age = parts[1].parse::<usize>();
+	if age.is_err() || *age.as_ref().unwrap() < 1 as usize {
+		return Person::default();
+	}
+	Person {
+		name: parts[0].to_string(),
+		age: age.unwrap(), 
+	}
+    }
 }
 
 fn main() {
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -48,6 +46,31 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s == "" {
+		return Err(ParsePersonError::Empty);
+	}
+	let mut parts = s.split(',');
+	let mut person = Person{age: 0, name: String::from("")};
+	if let Some(name) = parts.next() {
+	    if name == "" {
+		    return Err(ParsePersonError::NoName);
+	    }
+	    person.name = String::from(name);
+	} else {
+		return Err(ParsePersonError::BadLen);
+	}
+	if let Some(age_str) = parts.next() {
+	     match age_str.parse::<usize>() {
+		     Ok(age) => person.age = age,
+		     Err(e) => return Err(ParsePersonError::ParseInt(e))
+	     }
+	} else {
+		return Err(ParsePersonError::BadLen);
+	}
+	if let Some(_) = parts.next() {
+		return Err(ParsePersonError::BadLen);
+	}
+	Ok(person)
     }
 }
 
